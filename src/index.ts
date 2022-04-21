935ce7c4b838e1963cb99faa634cabf3af15d4d0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 16:41:54 +1000

    🎉 Update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
 
 import Vault from "./routes/Vault";
-import VaultUser from "./routes/VaultUser";
 
 const app = express();
 
 app.use("/vault", Vault);
-app.use("/vaultUser", VaultUser);
 
 const PORT = process.env.PORT || 7000;
 
