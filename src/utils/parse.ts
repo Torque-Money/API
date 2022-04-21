67eb0f9ecfb8cc1c9dce685fc2339c26058a1179
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 20:03:44 +1000

    🎉 Update

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,5 +1,11 @@
-// **** This is going to be useful for parsing different parts of our data
+import { ethers } from "ethers";
+
+import { ROUND_NUMBER } from "./constants";
 
 export function parsePercent(raw: string) {}
 
-export function parseBig(raw: number) {}
+export function parseNumber(raw: number) {}
+
+export function parseBigNumber(raw: ethers.BigNumber, decimals: number) {
+    return raw.mul(ROUND_NUMBER).div(ethers.BigNumber.from(10).pow(decimals)).toNumber() / ROUND_NUMBER;
+}
