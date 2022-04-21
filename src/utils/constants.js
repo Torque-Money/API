baeeb8262550c0e29c950cb9039d6b312ec2c284
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 21:33:19 +1000

    🎉 Update

--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BEEFY_API = exports.ROUND_NUMBER = void 0;
-exports.ROUND_NUMBER = 1e4;
-exports.BEEFY_API = "https://api.beefy.finance";
