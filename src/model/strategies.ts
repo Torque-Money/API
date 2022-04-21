379f03ea3f7a4a1c4e45bb7df491b4768a87f14a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 17:17:09 +1000

    🎉 Update

--- src/model/strategies.ts
+++ src/model/strategies.ts
@@ -1,2 +1,4 @@
 // **** This is what we are going to use to route strategies to their respective APY's and such
 // **** Each way of getting the API will need a different function (call to the beefy API) - therefore we need a way of routing it here
+
+// **** It is probably going to require multiple levels to search for this - we will dedicate each part to a specific part that will export all of its members and then we will search
