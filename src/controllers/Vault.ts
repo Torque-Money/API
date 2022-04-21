719d0d0ce8fcc9061876d936c56300eb545ec6a5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 16:39:25 +1000

    🎉 Update

--- src/controllers/Vault.ts
+++ src/controllers/Vault.ts
@@ -1,7 +1,10 @@
-// **** The handlers for the routes
+import { Request, Response } from "express";
 
 // Get the TVL of a particular vault
+function HandleVaultTVL(req: Request, res: Response) {}
 
 // Get the APY of a particular vault
+function HandleVaultAPY(req: Request, res: Response) {}
 
 // Get the fee for a particular vault
+function HandleVaultFee(req: Request, res: Response) {}
