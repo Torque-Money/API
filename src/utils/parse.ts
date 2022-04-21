1a895a0026ecd9ec01c7daf676990feed5fb8d31
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 20:06:56 +1000

    🎉 Update

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -4,7 +4,13 @@ import { ROUND_NUMBER } from "./constants";
 
 export function parsePercent(raw: string) {}
 
-export function parseNumber(raw: number) {}
+export function parseNumber(raw: number) {
+    if (raw > 1e9) {
+    } else if (raw > 1e6) {
+    } else if (raw > 1e3) {
+    } else {
+    }
+}
 
 export function parseBigNumber(raw: ethers.BigNumber, decimals: number) {
     return raw.mul(ROUND_NUMBER).div(ethers.BigNumber.from(10).pow(decimals)).toNumber() / ROUND_NUMBER;
