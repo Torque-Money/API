dad7b0d4f54d6f5db2bece9733fd3e4212c67a71
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 16:34:48 +1000

    🎉 Update

--- src/controllers/Vault.ts
+++ src/controllers/Vault.ts
@@ -1,1 +1,7 @@
 // **** The handlers for the routes
+
+// Get the TVL of a particular vault
+
+// Get the APY of a particular vault
+
+// Get the fee for a particular vault
--- src/routes/VaultUser.ts
+++ src/routes/VaultUser.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/tvl/:vault");
+router.get("/quote");
+router.get("/maxTokens");
+
 export default router;
