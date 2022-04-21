dc995ff81b1989637e6b195ad90b0051f1cb7cea
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 16:09:27 +1000

    🎉 Update

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import express from "express";
 const app = express();
 
 // **** How do I make handlers and routes and stuff again ?
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
