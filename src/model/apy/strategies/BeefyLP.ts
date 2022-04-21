e5937ba03777ae3e82b22f140c1b656b3f0bfa09
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 18:31:18 +1000

    🎉 Update

--- src/model/apy/strategies/BeefyLP.ts
+++ src/model/apy/strategies/BeefyLP.ts
@@ -1,1 +1,10 @@
-export function beefySpookyFTMUSDCLP() {}
+import axios from "axios";
+
+import { BEEFY_API } from "../../../utils/constants";
+
+export async function beefySpookyFTMUSDCLP() {
+    const url = `${BEEFY_API}/apy`;
+    const { data } = await axios.get<{ [key: string]: number }>(url);
+
+    return data["boo-ftm-usdc"] * 100;
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,3 @@
 export const ROUND_NUMBER = 1e4;
+
+export const BEEFY_API = "https://api.beefy.finance";
