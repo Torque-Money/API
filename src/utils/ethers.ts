ec67747f3f1386f0784205ab64368394bbb99655
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 21:19:58 +1000

    🎉 Update

--- src/utils/ethers.ts
+++ src/utils/ethers.ts
@@ -5,14 +5,12 @@ import TorqueVaultV1 from "../../abi/TorqueVaultV1.json";
 
 const RPC_URL = "https://rpc.ftm.tools/";
 
-export function loadContractTorqueVaultV1(vault: string) {
-    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+export const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
+export function loadContractTorqueVaultV1(vault: string) {
     return new ethers.Contract(vault, TorqueVaultV1.abi, provider);
 }
 
 export function loadContractBeefyLPStrategy(strategy: string) {
-    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
-
     return new ethers.Contract(strategy, BeefyLPStrategy.abi, provider);
 }
