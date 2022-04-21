a92597d41bb03d87904b202cc5fe9dd40638546d
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 19:21:49 +1000

    🎉 Update

--- src/service/Vault.ts
+++ src/service/Vault.ts
@@ -1,9 +1,16 @@
+import { loadContractVault } from "../utils/ethers";
+import getAPY from "./apy";
+
 // Get the TVL of a particular vault
 export async function getVaultTVL(vault: string) {}
 
 // Get the APY of a particular vault
 export async function getVaultAPY(vault: string) {
-    // **** Now we need a way of converting the vault to the strategy
+    const contractVault = loadContractVault(vault);
+
+    const strategy = await contractVault.getStrategy();
+
+    return getAPY(strategy);
 }
 
 // Get the fee for a particular vault
