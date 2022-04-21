e54cd0e307b72fe9512457cdc93d1d32e2c54f48
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 20:15:06 +1000

    🎉 Update

--- src/utils/ethers.ts
+++ src/utils/ethers.ts
@@ -5,12 +5,14 @@ import IVaultV1 from "../../abi/IVaultV1.json";
 
 const RPC_URL = "https://rpc.ftm.tools/";
 
-export const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
-
 export function loadContractVault(vault: string) {
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
     return new ethers.Contract(vault, IStrategy.abi, provider);
 }
 
 export function loadContractStrategy(strategy: string) {
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
     return new ethers.Contract(strategy, IVaultV1.abi, provider);
 }
