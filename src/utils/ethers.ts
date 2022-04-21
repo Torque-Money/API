5383961896bfdaa0a8a0f741babb9cdf76bfcd20
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 16:48:44 +1000

    🎉 Update

--- src/utils/ethers.ts
+++ src/utils/ethers.ts
@@ -3,5 +3,7 @@ import ethers from "ethers";
 export function loadEthers() {
     const RPC_URL = "https://rpc.ftm.tools/";
 
-    return new ethers.providers.JsonRpcProvider(RPC_URL);
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
+    return provider;
 }
