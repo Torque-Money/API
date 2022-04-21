f70b129379ca3aa3b197facf77ef6caabd084543
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 20:16:36 +1000

    🎉 Update

--- src/service/apy.ts
+++ src/service/apy.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { BEEFY_API } from "../utils/constants";
 
-const data: { [key: string]: () => Promise<number> } = {
+const vaults: { [key: string]: () => Promise<number> } = {
     "0x6Ad07E659563490d40377a98a7f0f62ed7d38C41": async () => {
         return await routeBeefyAPY("boo-ftm-usdc");
     },
@@ -16,5 +16,5 @@ async function routeBeefyAPY(name: string) {
 }
 
 export default async function getAPY(address: string) {
-    return await data[address]();
+    return await vaults[address]();
 }
