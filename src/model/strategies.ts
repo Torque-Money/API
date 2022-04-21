150eb67a26f782e77bba6c06494f01446be566d0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 17:10:00 +1000

    🎉 Update

---
+++ src/model/strategies.ts
@@ -0,0 +1,2 @@
+// **** This is what we are going to use to route strategies to their respective APY's and such
+// **** Each way of getting the API will need a different function (call to the beefy API) - therefore we need a way of routing it here
