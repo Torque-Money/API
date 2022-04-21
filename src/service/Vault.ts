3d19df342f6f13fd063272acd67955452eb4b0b3
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 16:32:34 +1000

    🎉 Update

--- src/service/Vault.ts
+++ src/service/Vault.ts
@@ -1,1 +1,7 @@
 // **** This is where we are going to make the low level calls to get the data
+
+// Get the TVL of a particular vault
+
+// Get the APY of a particular vault
+
+// Get the fee for a particular vault
