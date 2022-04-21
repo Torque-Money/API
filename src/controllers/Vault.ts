21edfe84580fa32911c27b1d1ba404d2271aaf05
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 16:51:10 +1000

    🎉 Update

--- src/controllers/Vault.ts
+++ src/controllers/Vault.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
+// **** Now I need to get the data from each of these, and then go through and parse it into JSON for the response
+
 // Get the TVL of a particular vault
 export async function HandleVaultTVL(req: Request, res: Response) {
     const vault = req.params.vault;
