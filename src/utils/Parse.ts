eb96d7a5624ed18c9850c50a0eeaaa14becf0e22
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-29 20:20:42 +1000

    🎉 Update

--- src/utils/Parse.ts
+++ src/utils/Parse.ts
@@ -3,17 +3,17 @@ import { BigNumber } from "ethers";
 import { ROUND_NUMBER } from "./";
 
 // Format a number to given decimal places
-export function formatNumber(raw: number) {
-    return raw.toFixed(2);
-}
+// export function formatNumber(raw: number) {
+//     return raw.toFixed(2);
+// }
 
 // Parse a number into its shortened form
-export function parseNumber(raw: number) {
-    if (raw > 1e9) return formatNumber(raw / 1e9) + "B";
-    else if (raw > 1e6) return formatNumber(raw / 1e6) + "M";
-    else if (raw > 1e3) return formatNumber(raw / 1e3) + "K";
-    else return formatNumber(raw);
-}
+// export function parseNumber(raw: number) {
+//     if (raw > 1e9) return formatNumber(raw / 1e9) + "B";
+//     else if (raw > 1e6) return formatNumber(raw / 1e6) + "M";
+//     else if (raw > 1e3) return formatNumber(raw / 1e3) + "K";
+//     else return formatNumber(raw);
+// }
 
 // Parse a big number into a number
 export function parseBigNumber(raw: BigNumber, decimals: number) {
