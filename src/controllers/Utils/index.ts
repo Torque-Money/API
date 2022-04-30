a1103b94f5f1d5e7fb1b818c8864841341ef7e33
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-30 10:25:04 +1000

    🎉 Update

--- src/controllers/Utils/index.ts
+++ src/controllers/Utils/index.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
+import { getTokenPrice } from "../../service/Utils";
 
 import { parseAddress } from "../../utils";
 
 // Takes in a token and returns its price
 export async function HandleTokenPrice(req: Request, res: Response) {
     const token = parseAddress(req.params.token);
+
+    const price = await getTokenPrice(token);
+
+    res.json({ price });
 }
