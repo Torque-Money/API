fe7be98cf4ca568f146a2d080f00e3be3dd745ed
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-30 10:23:55 +1000

    🎉 Update

--- src/service/Utils/_index.ts
+++ src/service/Utils/_index.ts
@@ -1,1 +1,6 @@
-export async function getTokenPrice(token: string) {}
+import { getTokenPrice as _getTokenPrice } from "../../utils";
+
+// Get the price for a given token
+export async function getTokenPrice(token: string) {
+    return await _getTokenPrice(token);
+}
