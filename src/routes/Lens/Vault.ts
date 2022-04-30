998ed697033a9dc14bd5e255a12802a01cbd03e6
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-30 11:48:42 +1000

    🎉 Update

--- src/routes/Lens/Vault.ts
+++ src/routes/Lens/Vault.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { HandleVaultAPY, HandleVaultFee, HandleVaultTVL } from "../../controllers/Lens";
+import { HandleVaultAPY, HandleVaultFee, HandleVaultTokens, HandleVaultTVL } from "../../controllers/Lens";
 import { routeWrapper } from "../../utils";
 
 const router = express.Router();
 
+router.get("/tokens/:vault", routeWrapper(HandleVaultTokens));
 router.get("/fee/:vault", routeWrapper(HandleVaultFee));
 router.get("/tvl/:vault", routeWrapper(HandleVaultTVL));
 router.get("/apy/:vault", routeWrapper(HandleVaultAPY));
