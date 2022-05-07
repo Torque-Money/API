f35ffac339189644e002bb6438944507911bb6bf
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-07 22:35:18 +1000

    🎉 Update

--- src/service/Lens/APY.ts
+++ src/service/Lens/APY.ts
@@ -3,16 +3,16 @@ import axios from "axios";
 import { BEEFY_API, parseAddress } from "../../utils";
 
 const strategies: { [key: string]: () => Promise<number> } = {
-    "0x529561Dfa18F6Ec726E5184579C018c202eA41EC": async () => {
+    "0x529561dfa18f6ec726e5184579c018c202ea41ec": async () => {
         return await routeBeefyAPY("sushi-usdc-wftm");
     },
-    "0x2439AF5Ff20Bd173f80a8c842d5bEa04E370EC57": async () => {
+    "0x2439af5ff20bd173f80a8c842d5bea04e370ec57": async () => {
         return await routeBeefyAPY("spirit-ftm-usdc");
     },
-    "0xd703B3B90E25f2D46f59F4578bDdca2C5A49c275": async () => {
+    "0xd703b3b90e25f2d46f59f4578bddca2c5a49c275": async () => {
         return await routeBeefyAPY("wigo-usdc-ftm");
     },
-    "0xEACcF1f6dC9f7f77cF81Fc00FF8bc77065778999": async () => {
+    "0xeaccf1f6dc9f7f77cf81fc00ff8bc77065778999": async () => {
         return await routeBeefyAPY("boo-ftm-usdc");
     },
 };
