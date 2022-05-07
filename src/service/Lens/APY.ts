ceed8383091b6cb87dca4a2f113fbc219cd27873
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-07 22:13:40 +1000

    🎉 Update

--- src/service/Lens/APY.ts
+++ src/service/Lens/APY.ts
@@ -3,7 +3,16 @@ import axios from "axios";
 import { BEEFY_API, parseAddress } from "../../utils";
 
 const strategies: { [key: string]: () => Promise<number> } = {
-    "0x6ad07e659563490d40377a98a7f0f62ed7d38c41": async () => {
+    "0x529561Dfa18F6Ec726E5184579C018c202eA41EC": async () => {
+        return await routeBeefyAPY("sushi-usdc-wftm");
+    },
+    "0x2439AF5Ff20Bd173f80a8c842d5bEa04E370EC57": async () => {
+        return await routeBeefyAPY("spirit-ftm-usdc");
+    },
+    "0xd703B3B90E25f2D46f59F4578bDdca2C5A49c275": async () => {
+        return await routeBeefyAPY("wigo-usdc-ftm");
+    },
+    "0xEACcF1f6dC9f7f77cF81Fc00FF8bc77065778999": async () => {
         return await routeBeefyAPY("boo-ftm-usdc");
     },
 };
