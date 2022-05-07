592e58c52df14786faa3392c1a4bbc67c7c405f6
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-07 21:59:24 +1000

    🎉 Update

--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,6 @@
-export { TorqueVaultV1 } from "./TorqueVaultV1";
-export { BeefyLPStrategy } from "./BeefyLPStrategy";
+export { ILens } from "./ILens";
+export { IRegistry } from "./IRegistry";
+export { IStrategy } from "./IStrategy";
+export { ISupportsFee } from "./ISupportsFee";
+export { ISupportsToken } from "./ISupportsToken";
+export { IVault } from "./IVault";
