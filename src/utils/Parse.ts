30e251e22513004dfcc06c6df7609d26e21f1ae6
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-10 19:20:50 +1000

    🎉 Update

--- src/utils/Parse.ts
+++ src/utils/Parse.ts
@@ -7,14 +7,6 @@ export function parseBigNumber(raw: BigNumber, decimals: number) {
     return raw.mul(ROUND_NUMBER).div(BigNumber.from(10).pow(decimals)).toNumber() / ROUND_NUMBER;
 }
 
-// Parse a number into a big number
-export function parseToBigNumber(raw: number, decimals: number) {
-    return BigNumber.from(10)
-        .pow(decimals)
-        .mul(Math.floor(raw * ROUND_NUMBER))
-        .div(ROUND_NUMBER);
-}
-
 // Parse an address for standardization
 export function parseAddress(address: string) {
     return address.toLowerCase();
